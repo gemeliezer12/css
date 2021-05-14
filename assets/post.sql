9b5f92bef5c71f28d4200d68061d26d885c47bd7
gemeliezer12 <72433644+gemeliezer12@users.noreply.github.com>
2021-05-15 07:47:18 +0800

    Update post.sql

--- assets/post.sql
+++ assets/post.sql
@@ -1,6 +1,10 @@
 CREATE TABLE posts(
 	idUsers int(11) NOT null,
     usernameUsers varchar(255) NOT null,
+    idPosts int (11) NOT null AUTO_INCREMENT
     contentPosts varchar(400),
-    mediaPosts varchar(255)
+    mediaPosts varchar(255),
+    likenumPosts int(11),
+    retweetnumPosts int(11),
+    commentnumPosts int(11)
 );
