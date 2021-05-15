d655f12fc0f966413827f3bcc4177dc2d4118584
gemeliezer12 <72433644+gemeliezer12@users.noreply.github.com>
2021-05-15 09:07:29 +0800

    Update post.sql

--- assets/post.sql
+++ assets/post.sql
@@ -1,10 +1,21 @@
 CREATE TABLE posts(
 	idUsers int(11) NOT null,
     usernameUsers varchar(255) NOT null,
-    idPosts int (11) NOT null AUTO_INCREMENT
-    contentPosts varchar(400),
+    idPosts int (11) NOT null AUTO_INCREMENT,
+    textPosts varchar(400),
     mediaPosts varchar(255),
     likenumPosts int(11),
     retweetnumPosts int(11),
     commentnumPosts int(11)
 );
+
+CREATE TABLE comments(
+    idUsers int(11) NOT null,
+    usernameUsers varchar(255) NOT null,
+    idPosts int(11) NOT null,
+    idComments int(11) NOT null PRIMARY KEY,
+    textComments varchar(400),
+    mediaComments varchar(255),
+    likenumComments int(11),
+    retweetnumComments int(11)
+)
